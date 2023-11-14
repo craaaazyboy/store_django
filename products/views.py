dade7757baa15fe7a9785342cf47370c1522594f
crazy_boy <i.kondratev2002@mail.ru>
2023-11-14 12:03:49 +0300

    crated baskets view with optional

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+ 
 # Create your views here.
-from products.models import FlowersWarehouse, FlowersCategory, FinishedProducts
+from products.models import FlowersWarehouse, FlowersCategory, FinishedProducts, Basket, FinishedProductsCategory
+from users.models import User
 
 def index(request):
     context = {
@@ -11,10 +14,39 @@ def index(request):
     return render(request, "products/index.html", context)
 
 
-def products(request):
+def products(request,category_id= None, page_number=1):
+    products = FinishedProducts.objects.filter(category_products_id=category_id) if category_id else FinishedProducts.objects.all()
+    per_page = 5
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
     context = {
         "title": "Flowers Store - Каталог",
-        "products": FlowersWarehouse.objects.all(),
-        "categories" : FlowersCategory.objects.all(),
+        "categories" : FinishedProductsCategory.objects.all(),
+        "products" : products_paginator,
     }
     return render(request, "products/products.html", context)
+
+@login_required
+def basket_add(request, product_id):
+    product = FinishedProducts.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets:
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.last()
+        basket.quantity += 1
+        basket.save()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+def basket_remove_all(request):
+    baskets = Basket.objects.filter(user=request.user)
+    for basket in baskets:
+        basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-
+from products.models import Basket
 
 def login(request):
     if request.method == 'POST':
@@ -33,6 +34,7 @@ def registration(request):
     context = {'form': form}
     return render(request, "users/registration.html",context)
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files= request.FILES)
@@ -43,7 +45,12 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+
+    context = {
+        'title': 'Store - Профиль',
+         'form': form,
+         'baskets': Basket.objects.filter(user=request.user),
+         }
     return render(request, 'users/profile.html',context)
 
 
